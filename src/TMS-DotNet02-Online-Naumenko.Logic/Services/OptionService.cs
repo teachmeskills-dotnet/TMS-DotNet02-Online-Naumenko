6b33c3ca8a27148fd5ecd55ba4de7b1e1aaa3495
Naumenko Dmitry <evilgodhedgehog@gmail.com>
2022-03-24 19:18:03 +0300

    fix: Fixed option controller

--- src/TMS-DotNet02-Online-Naumenko.Logic/Services/OptionService.cs
+++ src/TMS-DotNet02-Online-Naumenko.Logic/Services/OptionService.cs
@@ -1,13 +1,23 @@
-﻿using TMS_DotNet02_Online_Naumenko.Logic.Models;
+﻿using TMS_DotNet02_Online_Naumenko.Data.Models;
+using TMS_DotNet02_Online_Naumenko.Data.Repository;
+using TMS_DotNet02_Online_Naumenko.Logic.Mappers;
+using TMS_DotNet02_Online_Naumenko.Logic.Models;
 using TMS_DotNet02_Online_Naumenko.Logic.Services.Interfaces;
 
 namespace TMS_DotNet02_Online_Naumenko.Logic.Services
 {
     public class OptionService : IOptionService
     {
+        private readonly IRepository<Option> _optionRepository;
+
+        public OptionService(IRepository<Option> optionRepository)
+        {
+            _optionRepository = optionRepository ?? throw new ArgumentNullException(nameof(optionRepository));
+        }
+
         public IEnumerable<OptionDto> GetAll()
         {
-            throw new NotImplementedException();
+            return _optionRepository.GetAll().MapToDto();
         }
     }
 }
