04de3f714a3899478a1addf871eb616b0a38110a
Naumenko Dmitry <evilgodhedgehog@gmail.com>
2022-04-02 21:52:18 +0300

    feat: Added post info component

--- src/tms-dotnet02-online-naumenko.ui/src/components/Posts/PostInfo.jsx
+++ src/tms-dotnet02-online-naumenko.ui/src/components/Posts/PostInfo.jsx
@@ -1,59 +1,60 @@
 import React from 'react';
+import cl from './PostInfo.module.css';
 
 const PostInfo = (props) => {
     return (
-        <div className='cell-info px-4'>
-            <ul className='col-8 cell-info__date'>
-				<li className='nav-item'>
+        <div className={cl.post_info}>
+            <ul className={cl.post_info__date}>
+				<li className={cl.nav_item}>
                     {props.date}
                 </li>
-				<li className='nav-item d-flex ps-2' title='Просмотры'>
-                    <div className='nav-link'>
-                        <svg className='svg-icon'>
+				<li className={cl.nav_item} title='Просмотры'>
+                    <div className={cl.nav_link}>
+                        <svg className={cl.svg_icon}>
                             <use href='#icon-views_small'></use>
                         </svg>
                     </div>
                     <span>{props.views}</span>
                 </li>
 			</ul>
-			<ul className='col-4 cell-info__share'>
-                <li className='nav-item d-none d-lg-block'>
-					<div className='nav-link'>
+			<ul className={cl.post_info__share}>
+                <li className={cl.nav_item}>
+					<div className={cl.nav_link}>
 						<a href={'http://www.facebook.com/sharer.php?u=' + props.link} title='facebook'>
-                            <svg className='svg-icon'>
+                            <svg className={cl.svg_icon}>
                                 <use href='#social-facebook'></use>
                             </svg>
                         </a>
 					</div>
 				</li>
-				<li className='nav-item d-none d-lg-block'>
-					<div className='nav-link'>
+				<li className={cl.nav_item}>
+					<div className={cl.nav_link}>
                         <a href={'http://vkontakte.ru/share.php?url=' + props.link} title='vkontakte'>
-                            <svg className='svg-icon'>
+                            <svg className={cl.svg_icon}>
                                 <use href='#social-vkontakte'></use>
                             </svg>
                         </a>
 					</div>
 				</li>
-				<li className='nav-item d-none d-lg-block'>
-					<div className='nav-link'>
+				<li className={cl.nav_item}>
+					<div className={cl.nav_link}>
                         <a href={'http://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl=' + props.link} title='odnoklassniki'>
-                            <svg className='svg-icon'>
+                            <svg className={cl.svg_icon}>
                                 <use href='#social-odnoklassniki'></use>
                             </svg>
                         </a>
 					</div>
 				</li>
-                <li className='nav-item'>
-					<div className='nav-link'>
-                        <a className='share-more' onClick={() => props.handleToggle()}>
-                            <span className='share-more__desktop d-none d-lg-block'>
-                                <svg className='svg-icon'>
+                <li className={cl.nav_item}>
+					<div className={cl.nav_link}>
+                        <a onClick={() => props.handleToggle()}>
+                            <span className={cl.post_info__share_desktop}>
+                                <svg className={cl.svg_icon}>
                                     <use href='#icon-more'></use>
                                 </svg>
                             </span>
-                            <span className='share-more__mobile d-block d-lg-none'>
-                                <svg className='svg-icon'>
+                            <span className={cl.post_info__share_mobile}>
+                                <svg className={cl.svg_icon}>
                                     <use href='#icon-share'></use>
                                 </svg>
                             </span>
